054aabb7dfb82fecd4eab93b41a90bcceee3c673
svdiazr <saravdiazrg2309@gmail.com>
2026-03-19 07:43:59 -0500

    Update Auto.java

--- ejercicio dos/Auto.java
+++ ejercicio dos/Auto.java
@@ -31,6 +31,64 @@ public class Auto {
 		;
 	}
 
+	public String getMarca() {
+		return marca;
+	}
+
+
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+
+
+	public int getModelo() {
+		return modelo;
+	}
+
+
+
+	public void setModelo(int modelo) {
+		this.modelo = modelo;
+	}
+
+
+
+	public Motor getMotor() {
+		return motor;
+	}
+
+
+
+	public void setMotor(Motor motor) {
+		this.motor = motor;
+	}
+
+
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+
+
+	public List<Llanta> getListaLlantas() {
+		return listaLlantas;
+	}
+
+
+
+	public void setListaLlantas(List<Llanta> listaLlantas) {
+		this.listaLlantas = listaLlantas;
+	}
+
 	@Override
 	public String toString() {
 		return "Propietario del Vehiculo" + this.cliente + "Marca del auto:" + this.marca + " Modelo:" + this.modelo 
